73503f88bec945e5402eac69594220056c883817
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2024-09-03 00:08:27 +0530

    Time: 66 ms (10.8%), Space: 36 MB (6.92%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         int count = 0;
         map<int, int> mpp;
-        int sum = 0, remove = 0;
-        mpp[remove]++;
+        int sum = 0;
+        mpp[0] = 1;
         for(int i=0; i<n; i++){
             sum+=nums[i];
-            remove = sum % k;
+            int remove = sum % k;
             if(remove < 0){
                 remove += k;
             }
             count += mpp[remove];
-            mpp[remove] += 1;
+            mpp[remove]++;
         }
         return count;
     }
