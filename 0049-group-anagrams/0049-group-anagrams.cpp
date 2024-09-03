d94b77463ce3b3fa8f780f11428f62020c369aa5
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2024-09-04 01:17:20 +0530

    Time: 29 ms (51.92%), Space: 25 MB (58.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>>mpp;
-        for(int i=0; i<strs.size(); i++){
-            string key = strs[i];
-            sort(key.begin(), key.end());
-            mpp[key].push_back(strs[i]);
+        string word;
+        for(auto str:strs){
+            word = str;
+            sort(str.begin(), str.end());
+            mpp[str].push_back(word);
         }
         vector<vector<string>> res;
         for(auto entry : mpp){
