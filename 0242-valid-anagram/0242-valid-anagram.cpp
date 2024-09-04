a0fcb9753aa00f502c33df379fdc39bcce4638f6
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2024-09-05 01:45:10 +0530

    Time: 8 ms (63.5%), Space: 8.9 MB (53.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return(s == t);
+        int table[256] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            table[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            table[t[i]]--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (table[i] != 0)
+                return false;
+        }
+        return true;
     }
 };
