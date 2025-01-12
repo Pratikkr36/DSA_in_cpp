8fdb75cf640e8bd712c383dbbb826c3c520ed1f5
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2025-01-13 05:00:38 +0530

    Time: 20 ms (25.13%), Space: 30.2 MB (53.02%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -5,20 +5,23 @@ public:
         if(n%2 != 0)return false;
 
         int open = 0;
-        int close = 0;
         for(int i=0; i<n; i++){
             if(s[i]=='(' || locked[i]=='0'){
                 open++;
             }else open--;
 
             if(open<0)return false;
+        }
 
-            if(s[n-i-1]==')' || locked[n-i-1]=='0'){
+        int close = 0;
+        for(int i=n-1; i>=0; i--){
+            if(s[i]==')' || locked[i]=='0'){
                 close++;
             }else close--;
 
             if(close<0)return false;
         }
+
         return true;
     }
 };
