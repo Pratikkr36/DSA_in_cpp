d7824d115003ce211d77f2290f53fe54f16004b9
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2025-01-13 14:42:35 +0530

    Time: 4 ms (98.39%), Space: 32.1 MB (74.19%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -9,9 +9,6 @@ public:
         }
         for(int i=0; i<26; i++){
             ans += (count[i]%2 ? 1 : (count[i] ? 2 : 0));
-            // if(count[i]%2 != 0)ans+=1;
-            // else if(count[i] != 0)ans+=2;
-            // else ans+=0;
         }
         return ans;
     }
