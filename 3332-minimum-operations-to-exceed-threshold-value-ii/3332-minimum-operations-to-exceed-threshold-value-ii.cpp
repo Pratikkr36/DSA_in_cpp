38d8791396977a5f7b69b5f17caa2704856d4dfc
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2025-02-14 01:34:38 +0530

    Time: 99 ms (46.81%), Space: 88.8 MB (97.34%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        priority_queue<long long, vector<long long>, greater<long long>>pq(nums.begin(), nums.end());
+        priority_queue<int, vector<int>, greater<int>>pq(nums.begin(), nums.end());
         int count = 0;
-        while(pq.top()<k){
+        while(!pq.empty() && pq.top()<k){
             long long a = pq.top();
             pq.pop();
             long long b = pq.top();
             pq.pop();
-            a = a*2+b;
-            pq.push(a);
+            if(b<k && a<k && a*2+b<k){
+                pq.push(a*2+b);
+            }else{
+                pq.push(k);
+            }
             count++;
         }
         return count;
