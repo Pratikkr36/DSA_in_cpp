5b9dea4160cfa560dfac8ea682e69cfa4fae32d8
Pratik Mittal <93579834+Pratikkr36@users.noreply.github.com>
2025-07-25 23:33:57 +0530

    Time: 14 ms (61.54%), Space: 62.1 MB (5.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> mpp = new HashMap<>();
         Boolean result = false;
-        for(int i=0; i<nums.length -1; i++){
-            if(nums[i] == nums[i+1]){
+        for(int i=0; i<nums.length; i++){
+            if(mpp.containsKey(nums[i])){
                 return true;
             }
+            mpp.put(nums[i], i);
         }
         return result;
     }
